913cae1e164bf0b0456b3ca9b8a19a0f05573733
Araujo De Araujo Lauro Bezerra De <n134405@gruposantander.com>
2023-06-07 18:17:28 -0300

    Criação e inserção de contas

---
+++ mongo/script/semana2/insereContas.js
@@ -0,0 +1,59 @@
+use("ecomm");
+config.set('inspectDepth', 100);
+let insercoes = db.accounts.insertMany(
+    [
+        {
+            nome: "Taeltido",
+            email: "tael@gmail.com",
+            senha: "91111iko02",
+            dataCriacao: new Date(),
+            cpf: "95301006045",
+            telefone: "(51) 9090-2999",
+            endereco: {
+                rua: "Rua São Pedro",
+                bairro: "Nova República",
+                numero: "911",
+                cep: "65090385",
+                cidade: "São Luís",
+                uf: "MA",
+                complemento: null
+            }
+        },
+        {
+            nome: "Joao Pedro",
+            email: "pedroJoao@gmail.com",
+            senha: "iiii111199je",
+            dataCriacao: new Date(),
+            cpf: "82800439009",
+            telefone: "(51) 2323-2999",
+            endereco: {
+                rua: "Rua Maranatha",
+                bairro: "Petrópolis",
+                numero: "91334",
+                cep: "57062005",
+                cidade: "Maceió",
+                uf: "AL",
+                complemento: "Complemento adicionado"
+            }
+        },
+        {
+            nome: "Eugenio",
+            email: "eugen@gmail.com",
+            senha: "n111i1i1iw",
+            dataCriacao: new Date(),
+            cpf: "98241640064",
+            telefone: "(51) 9440-2329",
+            endereco: {
+                rua: "Rua Austriclínio Ferraz",
+                bairro: "Tejipió",
+                numero: "223",
+                cep: "50920340",
+                cidade: "Recife",
+                uf: "PE",
+                complemento: null
+            }
+        }
+    ]
+);
+
+console.log(insercoes);
